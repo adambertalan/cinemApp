7e9551ad820680a0e12bfb3d414067b9e54ff6a5
szanicsl <szanicsl@gmail.com>
2016-11-03 23:04:13 +0100

    Navigation with from-action.

---
+++ CinemApp-web/src/main/java/hu/unideb/rft/beadando/cinemapp/web/managedbean/NavigationController.java
@@ -0,0 +1,36 @@
+package hu.unideb.rft.beadando.cinemapp.web.managedbean;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class NavigationController implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public String processHome() {
+        return "page";
+    }
+
+    public String processMovies() {
+        return "page";
+    }
+
+    public String processContact() {
+        return "page";
+    }
+
+    public String processGYIK() {
+        return "page";
+    }
+
+    public String processLogin() {
+        return "page";
+    }
+
+    public String processAdmin() {
+        return "page";
+    }
+}
