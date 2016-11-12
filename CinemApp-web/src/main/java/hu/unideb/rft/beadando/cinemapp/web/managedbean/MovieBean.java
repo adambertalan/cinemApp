5d233e9718f502a3958b2a701ad282414709728c
szanicsl <szanicsl@gmail.com>
2016-11-12 22:21:21 +0100

    Poster uploading for the selected movie.

--- CinemApp-web/src/main/java/hu/unideb/rft/beadando/cinemapp/web/managedbean/MovieBean.java
+++ CinemApp-web/src/main/java/hu/unideb/rft/beadando/cinemapp/web/managedbean/MovieBean.java
@@ -2,7 +2,7 @@
 * To change this license header, choose License Headers in Project Properties.
 * To change this template file, choose Tools | Templates
 * and open the template in the editor.
-*/
+ */
 package hu.unideb.rft.beadando.cinemapp.web.managedbean;
 
 import java.util.List;
@@ -14,174 +14,215 @@ import javax.faces.bean.ViewScoped;
 
 import hu.unideb.rft.beadando.cinemapp.ejb.api.MovieService;
 import hu.unideb.rft.beadando.cinemapp.jpa.entity.Movie;
+import java.io.IOException;
+import javax.faces.context.FacesContext;
+
+import javax.servlet.http.Part;
 
 @ManagedBean(name = "movieBean")
 @ViewScoped
 public class MovieBean {
 
-	@EJB
-	private MovieService movieService;
+    @EJB
+    private MovieService movieService;
 
-	private Long selectedMovieId;
+    private Long selectedMovieId;
 
-	private String movieName;
-	private String movieCode;
-	private String movieDescription;
-	private Integer movieAgeLimit;
-	private Long movieGenreId;
-	private Integer movieLength;
-	
+    private String movieName;
+    private String movieCode;
+    private String movieDescription;
+    private Integer movieAgeLimit;
+    private Long movieGenreId;
+    private Integer movieLength;
 
-	private static Long movieToBeEditedID = null;
-	
-	private List<Movie> movies;
+    private static Long movieToBeEditedID = null;
 
-	@PostConstruct
-	public void init() {
-		System.out.println("called");
+    private List<Movie> movies;
+
+    private Part file;
+
+    private String getFileNameWithPath(Part part) {
+        String extension = "";
+        for (String cd : part.getHeader("content-disposition").split(";")) {
+            if (cd.trim().startsWith("filename")) {
+                String filename = cd.substring(cd.indexOf('=') + 1).trim().replace("\"", "");
+                extension = filename.substring(filename.lastIndexOf('/') + 1).substring(filename.lastIndexOf('\\') + 1).substring(filename.lastIndexOf(".") + 1);
+            }
+        }
+        String path = FacesContext.getCurrentInstance().getExternalContext().getRealPath("resources\\images\\");
+
+        return path + "/" + movieCode + "." + extension;
+    }
+
+    public String upload() throws IOException {
+        Movie selectedMovie = movieService.getMovieRepository().findMovieById(selectedMovieId);
+        if (selectedMovie != null) {
+            movieName = selectedMovie.getName();
+            movieCode = selectedMovie.getMovieCode();
+            movieAgeLimit = selectedMovie.getAgeLimit();
+            movieLength = selectedMovie.getLength();
+            movieGenreId = selectedMovie.getGenre().getId();
+            movieDescription = selectedMovie.getDescription();
+            movieToBeEditedID = selectedMovieId;
+        }
+        System.out.println(getFileNameWithPath(file));
+        file.write(getFileNameWithPath(file));
+        return "success";
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("called");
 //		FacesContextUtils.getRequiredWebApplicationContext(FacesContext.getCurrentInstance())
 //				.getAutowireCapableBeanFactory().autowireBean(this);
-		movies = movieService.findAllMovies();
-		System.out.println("Movies :" + movies);
-	}
+        movies = movieService.findAllMovies();
+        System.out.println("Movies :" + movies);
+    }
 
-	public void addNewMovie() {
-		System.out.println(movieName);
-		System.out.println(movieCode);
-		System.out.println(movieDescription);
-		System.out.println(movieAgeLimit);
-		System.out.println(movieGenreId);
-		System.out.println(movieLength);
-		
-		if(movieToBeEditedID == null){
-			addMovie();
-		}else{
-			Movie editedMovie = movieService.getMovieRepository().findMovieById(movieToBeEditedID);
-			if(editedMovie == null){
-				addMovie();
-				clearTextFields();
-				return;
-			}
-			editedMovie.setName(movieName);
-			editedMovie.setMovieCode(movieCode);
-			editedMovie.setDescription(movieDescription);
-			editedMovie.setAgeLimit(movieAgeLimit);
-			editedMovie.setGenre(movieService.getGenreRepository().findOne(movieGenreId));
-			editedMovie.setLength(movieLength);
-			movieService.getMovieRepository().save(editedMovie);
-			movies = movieService.findAllMovies();
-			movieToBeEditedID = null;
-		}
+    public void addNewMovie() throws IOException {
+        System.out.println(movieName);
+        System.out.println(movieCode);
+        System.out.println(movieDescription);
+        System.out.println(movieAgeLimit);
+        System.out.println(movieGenreId);
+        System.out.println(movieLength);
 
-		clearTextFields();
-	}
-	
-	private void clearTextFields(){
-		this.movieName = null;
-		this.movieCode = null;
-		this.movieDescription = null;
-		this.movieAgeLimit = null;
-		this.movieLength = null;
-		this.movieGenreId = 1L;
-	}
-	
-	private void addMovie(){
-		Movie movie = movieService.createMovie(movieName, movieCode, movieAgeLimit, movieDescription, movieLength,
-				movieGenreId);
-		movies.add(movie);
-	}
+        if (movieToBeEditedID == null) {
+            addMovie();
+        } else {
+            Movie editedMovie = movieService.getMovieRepository().findMovieById(movieToBeEditedID);
+            if (editedMovie == null) {
+                addMovie();
+                clearTextFields();
+                return;
+            }
+            editedMovie.setName(movieName);
+            editedMovie.setMovieCode(movieCode);
+            editedMovie.setDescription(movieDescription);
+            editedMovie.setAgeLimit(movieAgeLimit);
+            editedMovie.setGenre(movieService.getGenreRepository().findOne(movieGenreId));
+            editedMovie.setLength(movieLength);
+            movieService.getMovieRepository().save(editedMovie);
+            movies = movieService.findAllMovies();
+            movieToBeEditedID = null;
+        }
 
-	public void editMovie() {
-		
-			Movie selectedMovie = movieService.getMovieRepository().findMovieById(selectedMovieId);
-			if(selectedMovie != null){
-				movieName = selectedMovie.getName();
-				movieCode = selectedMovie.getMovieCode();
-				movieAgeLimit = selectedMovie.getAgeLimit();
-				movieLength = selectedMovie.getLength();
-				movieGenreId = selectedMovie.getGenre().getId();
-				movieDescription = selectedMovie.getDescription();
-				movieToBeEditedID = selectedMovieId;
-			}
-	}
+        clearTextFields();
+    }
 
-	public void deleteMovie() {
-		
-		movieService.deleteMovie(selectedMovieId);
-		movies = movieService.findAllMovies();
+    private void clearTextFields() {
+        this.movieName = null;
+        this.movieCode = null;
+        this.movieDescription = null;
+        this.movieAgeLimit = null;
+        this.movieLength = null;
+        this.movieGenreId = 1L;
+    }
 
-	}
+    private void addMovie() {
+        Movie movie = movieService.createMovie(movieName, movieCode, movieAgeLimit, movieDescription, movieLength,
+                movieGenreId);
+        movies.add(movie);
+    }
 
-	public MovieService getMovieService() {
-		return movieService;
-	}
+    public void editMovie() {
 
-	public void setMovieService(MovieService movieService) {
-		this.movieService = movieService;
-	}
+        Movie selectedMovie = movieService.getMovieRepository().findMovieById(selectedMovieId);
+        if (selectedMovie != null) {
+            movieName = selectedMovie.getName();
+            movieCode = selectedMovie.getMovieCode();
+            movieAgeLimit = selectedMovie.getAgeLimit();
+            movieLength = selectedMovie.getLength();
+            movieGenreId = selectedMovie.getGenre().getId();
+            movieDescription = selectedMovie.getDescription();
+            movieToBeEditedID = selectedMovieId;
+        }
+    }
 
-	public String getMovieName() {
-		return movieName;
-	}
+    public void deleteMovie() {
 
-	public void setMovieName(String movieName) {
-		this.movieName = movieName;
-	}
+        movieService.deleteMovie(selectedMovieId);
+        movies = movieService.findAllMovies();
 
-	public String getMovieCode() {
-		return movieCode;
-	}
+    }
 
-	public void setMovieCode(String movieCode) {
-		this.movieCode = movieCode;
-	}
+    public MovieService getMovieService() {
+        return movieService;
+    }
 
-	public String getMovieDescription() {
-		return movieDescription;
-	}
+    public void setMovieService(MovieService movieService) {
+        this.movieService = movieService;
+    }
 
-	public void setMovieDescription(String movieDescription) {
-		this.movieDescription = movieDescription;
-	}
+    public String getMovieName() {
+        return movieName;
+    }
 
-	public Integer getMovieAgeLimit() {
-		return movieAgeLimit;
-	}
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
 
-	public void setMovieAgeLimit(Integer movieAgeLimit) {
-		this.movieAgeLimit = movieAgeLimit;
-	}
+    public String getMovieCode() {
+        return movieCode;
+    }
 
-	public Long getMovieGenreId() {
-		return movieGenreId;
-	}
+    public void setMovieCode(String movieCode) {
+        this.movieCode = movieCode;
+    }
 
-	public void setMovieGenreId(Long movieGenreId) {
-		this.movieGenreId = movieGenreId;
-	}
+    public String getMovieDescription() {
+        return movieDescription;
+    }
 
-	public List<Movie> getMovies() {
-		return movies;
-	}
+    public void setMovieDescription(String movieDescription) {
+        this.movieDescription = movieDescription;
+    }
 
-	public void setMovies(List<Movie> movies) {
-		this.movies = movies;
-	}
+    public Integer getMovieAgeLimit() {
+        return movieAgeLimit;
+    }
 
-	public Integer getMovieLength() {
-		return movieLength;
-	}
+    public void setMovieAgeLimit(Integer movieAgeLimit) {
+        this.movieAgeLimit = movieAgeLimit;
+    }
 
-	public void setMovieLength(Integer movieLength) {
-		this.movieLength = movieLength;
-	}
+    public Long getMovieGenreId() {
+        return movieGenreId;
+    }
 
-	public Long getSelectedMovieId() {
-		return selectedMovieId;
-	}
+    public void setMovieGenreId(Long movieGenreId) {
+        this.movieGenreId = movieGenreId;
+    }
 
-	public void setSelectedMovieId(Long selectedMovieId) {
-		this.selectedMovieId = selectedMovieId;
-	}
-	
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public Integer getMovieLength() {
+        return movieLength;
+    }
+
+    public void setMovieLength(Integer movieLength) {
+        this.movieLength = movieLength;
+    }
+
+    public Long getSelectedMovieId() {
+        return selectedMovieId;
+    }
+
+    public void setSelectedMovieId(Long selectedMovieId) {
+        this.selectedMovieId = selectedMovieId;
+    }
+
+    public Part getFile() {
+        return file;
+    }
+
+    public void setFile(Part file) {
+        this.file = file;
+    }
 }
