2e962c5a1b0e948003100c6f40c5e92396890477
Nándor Szabo <n.szabo@bssn-software.de>
2016-11-17 14:53:58 +0100

    Try to solve new genre bug.

--- CinemApp-ejb/src/main/java/hu/unideb/rft/beadando/cinemapp/ejb/impl/GenreServiceImpl.java
+++ CinemApp-ejb/src/main/java/hu/unideb/rft/beadando/cinemapp/ejb/impl/GenreServiceImpl.java
@@ -2,15 +2,16 @@ package hu.unideb.rft.beadando.cinemapp.ejb.impl;
 
 import java.util.List;
 
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.interceptor.Interceptors;
 import javax.transaction.Transactional;
 import javax.transaction.Transactional.TxType;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ejb.interceptor.SpringBeanAutowiringInterceptor;
 
 import hu.unideb.rft.beadando.cinemapp.ejb.api.GenreService;
+import hu.unideb.rft.beadando.cinemapp.ejb.api.MovieService;
 import hu.unideb.rft.beadando.cinemapp.jpa.entity.Genre;
 import hu.unideb.rft.beadando.cinemapp.jpa.repository.GenreRepository;
 
@@ -19,24 +20,24 @@ import hu.unideb.rft.beadando.cinemapp.jpa.repository.GenreRepository;
 @Interceptors({ SpringBeanAutowiringInterceptor.class })
 public class GenreServiceImpl implements GenreService {
 	
-	@Autowired
-    private GenreRepository genreRepository;
+	@EJB
+	private MovieService movieService;
 
 	@Override
 	public List<Genre> findAllGenre() {
-		return genreRepository.findAll();
+		return movieService.getGenreRepository().findAll();
 	}
 
 	@Override
 	public GenreRepository getGenreRepository() {
-		return genreRepository;
+		return movieService.getGenreRepository();
 	}
 
 	@Override
 	public Genre createGenre(String name) {
 		Genre genre = new Genre();
 		genre.setName(name);
-		genreRepository.save(genre);
+		movieService.getGenreRepository().save(genre);
 		return genre;
 	}
 
