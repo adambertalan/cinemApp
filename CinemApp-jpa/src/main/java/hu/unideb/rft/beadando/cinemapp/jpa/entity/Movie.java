af5d5a5c729dfa4ba70fd539662aaf20861d8dc1
Nándor Szabo <n.szabo@bssn-software.de>
2016-12-09 13:52:09 +0100

    Nullpointer rateCounternél javítása.

--- CinemApp-jpa/src/main/java/hu/unideb/rft/beadando/cinemapp/jpa/entity/Movie.java
+++ CinemApp-jpa/src/main/java/hu/unideb/rft/beadando/cinemapp/jpa/entity/Movie.java
@@ -11,7 +11,7 @@ public class Movie extends BaseName {
    
     private Double rating;
     
-    private Integer rateCounter;
+    private Integer rateCounter = 0;
     
     private String movieCode;
     
